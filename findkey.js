d04c590a14e6fccbbe1fb32b7af99b5442c81cda
Carol Cheung <carol.cheung@telus.com>
2022-11-17 21:11:34 -0800

    add findKey

--- findkey.js
+++ findkey.js
@@ -6,12 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// loop through obj, 
+// each iteration of the key of the obj
+// test key against callback function stars === 2
+// if match is found stop and return result
+
 const findKey = function(obj, callback) {
-  for (let key in obj) {
-   if (callback(obj[key])) { 
-    return key
-   }
-  }
+  let result = "";
+for (let key in obj) {
+  if (callback(obj[key]))         //callback executed here obj[key] = {stars:2} and testing key
+    return = key;
+    break;
+  } 
+return result; 
 };
 
 assertEqual(findKey({
@@ -21,4 +28,4 @@ assertEqual(findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
+}, x => x.stars === 2), "noma"); // => "noma"
